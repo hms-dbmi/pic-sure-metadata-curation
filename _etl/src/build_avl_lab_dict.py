123fc7b0050804c40dc240ed71560abfca8e5bfe
thomas_desain1 <thomas_desain@hms-srce.org>
2025-05-14 10:00:59 -0400

    Update variable_id logic to use var_label if available
    Allow variable_id to default to var_label when provided, enhancing
    clarity and aligning with expected labeling behavior.

--- _etl/src/build_avl_lab_dict.py
+++ _etl/src/build_avl_lab_dict.py
@@ -82,11 +82,11 @@ def build_metadata_json(study_id, abv_name, alt_name, output_dir, input_dir, sch
 
     variables = [
         {
-            "variable_id": (var_label or var_id),
-            "variable_name": var_id,
+            "variable_id": var_id,
+            "variable_name": var_label,
             "variable_type": "num",
             "variable_description": var_label,
-            "data_hierarchy": "\\" + study_phs_number + "\\" + (var_label or var_id) + "\\",
+            "data_hierarchy": "\\" + study_phs_number + "\\" + var_id + "\\",
             "drs_uri": drs_uris
         }
         for var_id, var_label in column_names_to_labels.items()
